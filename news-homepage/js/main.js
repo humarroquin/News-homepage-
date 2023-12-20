a24bcf57cbabdf5303802cb3e5b6e824053eff26
Hugo Marroquin <149089365+humarroquin@users.noreply.github.com>
2023-12-19 19:31:30 -0600

    Update main.js

--- news-homepage/js/main.js
+++ news-homepage/js/main.js
@@ -2,24 +2,22 @@
 
 // mobile menu
 document.addEventListener('DOMContentLoaded', () => {
-  const hamburgerBtn = document.querySelector('.hamburger-button');
-  const mobileMenu = document.querySelector('.mobile-menu');
+  const hamburgerBtn = document.querySelector('.hamburger-btn');
+  const mobileMenu = document.querySelector('.primary-navigation');
   const overlay = document.querySelector('.overlay');
 
-  hamburgerBtn.addEventListener('click', () => {
+  const openMobileMenu = function () {
     mobileMenu.classList.toggle('active');
     overlay.classList.toggle('hidden');
-  });
+  };
 
-  overlay.addEventListener('click', () => {
-    mobileMenu.classList.toggle('active');
-    overlay.classList.toggle('hidden');
-  });
+  hamburgerBtn.addEventListener('click', openMobileMenu);
+
+  overlay.addEventListener('click', openMobileMenu);
 
   document.addEventListener('keydown', function (e) {
     if (e.key === 'Escape' && mobileMenu.classList.contains('active')) {
-      mobileMenu.classList.remove('active');
-      overlay.classList.add('hidden');
+      openMobileMenu();
     }
   });
 });
